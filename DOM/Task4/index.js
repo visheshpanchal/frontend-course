c6257b5d6d29648193ed860f93c07a570a5423b2
Vishesh Panchal <visheshpanchal145@mail.com>
2022-08-28 21:09:12 +0530

    Task 4  Done

---
+++ DOM/Task4/index.js
@@ -0,0 +1,8 @@
+let cc = document.getElementsByClassName("list-group-item");
+
+cc[2].style.color = "green";
+
+for (let index = 0; index < cc.length; index++) {
+    cc[index].style.fontWeight = "bold";
+    
+}
